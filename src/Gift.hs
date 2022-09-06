3a96848f2243605ec5f4fe530504267495856016
SolidSnake <solid@snake>
2022-09-06 16:08:44 -0400

    fitting code to plutus apps v0.1.0

--- src/Gift.hs
+++ src/Gift.hs
@@ -1,71 +1,121 @@
-module Gift where
+module Gift (validator, valHash, scrAddress, runEmulator)
+where
 
+import           Prelude               hiding (($), (<$>))
 import           Control.Monad       hiding (fmap)
 import           Data.Map            as Map
 import           Data.Text           (Text)
 import           Data.Void           (Void)
-import           Plutus.Contract
-import           PlutusTx            (Data (..))
-import qualified PlutusTx
+
+import           Plutus.Contract    as Contract
+import           PlutusTx           as PlutusTx
 import qualified PlutusTx.Builtins   as Builtins
-import           PlutusTx.Prelude    hiding (Semigroup(..), unless)
-import           Ledger              hiding (singleton)
-import           Ledger.Constraints  as Constraints
-import qualified Ledger.Typed.Scripts      as Scripts
-import  qualified Plutus.V1.Ledger.Scripts as Plutus
+import           PlutusTx.Prelude    as PlutusTxPrelude hiding (Semigroup(..), unless, (.))
+
+import           Ledger              as Ledger hiding (singleton)
+import qualified Ledger.Constraints  as Constraints
+
+import qualified Plutus.Script.Utils.V1.Scripts as UtilsScripts
+import qualified Plutus.V1.Ledger.Scripts as LedgerScripts
+import qualified Plutus.V1.Ledger.Api as LedgerApi
 import           Ledger.Ada          as Ada
+
 import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
 import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
 import           Playground.Types    (KnownCurrency (..))
-import           Prelude             (IO, Semigroup (..), String)
 import           Text.Printf         (printf)
 
+import Plutus.Trace as PlutusTrace
+import Wallet.Emulator as WalletEmulator
+import qualified Control.Monad.Freer.Extras as Extras
+import Data.Default
+import qualified Data.Aeson as DataAeson
+
+
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
+--------------------------
+-- On Chain Code
+--------------------------
 
 {-# INLINABLE mkValidator #-}
 mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-mkValidator _ _ _ = ()
-{-
-validator :: Scripts.Validator
-validator = Plutus.mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+--mkValidator _ _ _ = ()
+mkValidator _ _ _ = trace "Hello from on chain code"()
 
-valHash :: Plutus.ValidatorHash
-valHash = Plutus.getValidator validator
+--------------------------
+-- Helper Functions
+--------------------------
+
+validator :: LedgerScripts.Validator
+validator = LedgerScripts.mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+
+valHash :: UtilsScripts.ValidatorHash
+valHash = UtilsScripts.validatorHash validator
 
 scrAddress :: Ledger.Address
-scrAddress = scriptAddress validator
--}
-{-
+scrAddress = Ledger.scriptAddress validator
+
+--------------------------
+-- Off Chain Code - PAB
+--------------------------
+
 type GiftSchema =
             Endpoint "give" Integer
         .\/ Endpoint "grab" ()
 
 give :: AsContractError e => Integer -> Contract w s e ()
 give amount = do
-    let tx = mustPayToOtherScript valHash (Plutus.Datum $ Builtins.mkI 0) $ Ada.lovelaceValueOf amount
-    ledgerTx <- submitTx tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-    logInfo @String $ printf "made a gift of %d lovelace" amount
+    let tx = Constraints.mustPayToOtherScript valHash (LedgerApi.Datum $ Builtins.mkI 0) $ Ada.lovelaceValueOf amount
+    ledgerTx <- Contract.submitTx tx
+    void $ Contract.awaitTxConfirmed $ Ledger.getCardanoTxId ledgerTx
+    --void $ Contract.toContract $ Contract.throwError @Text "" 
+    Contract.logInfo @String $ printf "made a gift of %d lovelace" amount
 
 grab :: forall w s e. AsContractError e => Contract w s e ()
 grab = do
-    utxos <- utxosAt scrAddress
-    let orefs   = fst <$> Map.toList utxos
+    utxos <- Contract.utxosAt scrAddress
+    let orefs   = PlutusTxPrelude.fst <$> Map.toList utxos
+        chainIndex = PlutusTxPrelude.snd <$> Map.toList utxos
         lookups = Constraints.unspentOutputs utxos      <>
                   Constraints.otherScript validator
-        tx :: TxConstraints Void Void
-        tx      = mconcat [mustSpendScriptOutput oref $ Plutus.Redeemer $ Builtins.mkI 17 | oref <- orefs]
-    ledgerTx <- submitTxConstraintsWith @Void lookups tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-    logInfo @String $ "collected gifts"
+        tx :: Constraints.TxConstraints Void Void
+        tx      = PlutusTxPrelude.mconcat [Constraints.mustSpendScriptOutput oref $ LedgerApi.Redeemer $ Builtins.mkI 17 | oref <- orefs]
+
+    Contract.logInfo @String $ "Beginning log TxOutRef"
+    PlutusTxPrelude.sequence_ $ (logInfo @String . show) <$> orefs 
+    PlutusTxPrelude.sequence_ $ logInfo @TxOutRef <$> orefs 
+    Contract.logInfo @String $ "End log TxOutRef"
+
+    Contract.logInfo @String $ "Beginning log chainIndex"
+    PlutusTxPrelude.sequence_ $ (logInfo @String . show) <$> chainIndex 
+    PlutusTxPrelude.sequence_ $ logInfo @ChainIndexTxOut <$> chainIndex 
+    Contract.logInfo @String $ "End log chainIndex"
+
+    ledgerTx <- Contract.submitTxConstraintsWith @Void lookups tx
+    void $ Contract.awaitTxConfirmed $ Ledger.getCardanoTxId ledgerTx
+    Contract.logInfo @String $ "collected gifts"
 
 endpoints :: Contract () GiftSchema Text ()
-endpoints = awaitPromise (give' `select` grab') >> endpoints
+endpoints = Contract.awaitPromise (give' `select` grab') >> endpoints
   where
-    give' = endpoint @"give" give
-    grab' = endpoint @"grab" $ const grab
+    give' = Contract.endpoint @"give" give
+    grab' = Contract.endpoint @"grab" $ PlutusTxPrelude.const grab
 
 mkSchemaDefinitions ''GiftSchema
 
 mkKnownCurrencies []
--}
+
+--------------------------
+-- Emulator Trace
+--------------------------
+
+runEmulator = runEmulatorTraceIO' def def myTrace
+
+myTrace = do
+    handle1 <- PlutusTrace.activateContractWallet (WalletEmulator.knownWallet 1) endpoints
+    handle2 <- PlutusTrace.activateContractWallet (WalletEmulator.knownWallet 2) endpoints
+    callEndpoint @"give" handle1 10000000
+    void $ PlutusTrace.waitUntilSlot 2
+    PlutusTrace.callEndpoint @"grab" handle1 ()
+    s <- PlutusTrace.waitNSlots 2
+    Extras.logDebug $ "reached -> " <> show s
